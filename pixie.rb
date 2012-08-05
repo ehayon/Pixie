413952e2dc0d7727e679ef368bbe68add3cfea6a
Ethan Hayon <ehayon@gmail.com>
2012-08-05 17:07:02 -0400

    renamed project file to pixie

--- pixie.rb
+++ pixie.rb
@@ -11,21 +11,21 @@ class Image
   def to_ascii
 	chars = %w(;; $$ @@ ,, ^^ __ %% ** (* *) ==)
 	chars[11] = "  "
-  	p = []
-	scaled = self.image.scale(0.4).quantize(12)
+	p = []
+	scaled = self.image.scale(100,100).quantize(12)
 	scaled.each_pixel { |pixel| p << pixel }
 	p = p.uniq.sort.map(&:intensity)
 	ascii = []
 	#binding.pry
 	scaled.each_pixel do |pixel, c, r|
-		print chars[p.index(pixel.intensity)]
-		puts if c == scaled.columns - 1
-		ascii << chars[p.index(pixel.intensity)]
-		ascii << "\n" if c == scaled.columns - 1
+	  print chars[p.index(pixel.intensity)]
+	  puts if c == scaled.columns - 1
+	  ascii << chars[p.index(pixel.intensity)]
+	  ascii << "\n" if c == scaled.columns - 1
 	end
-  	ascii
-  end
-end
+	  ascii
+	end
+	end
 
 image = Image.new('image.png')
 File.open('output.txt', 'w') do |f|
