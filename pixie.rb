5e5a685e2c19392d1d851cd4d0b0a7de1e21317e
Ethan Hayon <ehayon@gmail.com>
2012-08-05 22:27:09 -0400

    play around with the ascii characters

--- pixie.rb
+++ pixie.rb
@@ -10,24 +10,24 @@ class Image
 
   def to_ascii
 	#chars = (0..11).map { rand(33..126).chr } # playing around with a random ascii character set
-	chars = %w(! @ # $ % ^ & * : ' ; .)
+	chars = %w( % H m A v @ y < # $ > % ^ o * : ' ` .)
 	p = []
-	scaled = self.image.scale(100,100).quantize(chars.length)
-	scaled.each_pixel { |pixel| p << pixel }
-	p = p.uniq.sort.map(&:intensity)
+	scaled = self.image.scale(0.2).quantize(chars.length)
+	scaled.each_pixel { |pixel| p << pixel.intensity }
+	p = p.uniq.sort
 	chars[p.length-1] = " "
 	ascii = []
-	#binding.pry
+	# binding.pry
 	scaled.each_pixel do |pixel, c, r|
+	  # binding.pry
 	  print chars[p.index(pixel.intensity)] + " "
 	  puts if c == scaled.columns - 1
-	  ascii << chars[p.index(pixel.intensity)]
+	  ascii << chars[p.index(pixel.intensity)] + " "
 	  ascii << "\n" if c == scaled.columns - 1
 	end
 	  ascii
 	end
 	end
-
 image = Image.new('image.png')
 File.open('output.txt', 'w') do |f|
   f.print(image.to_ascii.join)
